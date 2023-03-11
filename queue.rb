4f7b45ad89b7c8b47b59649c16dded1c664659d1
drago-lubuntu <pedrodrago.profissional@gmail.com>
2023-03-10 21:20:27 -0300

    a

--- queue.rb
+++ queue.rb
@@ -1,12 +1,11 @@
 class Node
   attr_accessor :value, :next
+
   def initialize(value)
     @value = value
     @next = nil
   end
 end
-
-
 
 class Queue
   def initialize
@@ -21,36 +20,30 @@ class Queue
 
   def enqueue(item)
     node = Node.new(item)
-    if @length == 0
+    if @length.zero?
       @tail = node
       @head = node
-      @length+=1
+      @length += 1
     else
       @tail.next = node
       @tail = node
     end
-  
   end
 
   def deque
     if !head?
       nil
     else
-      @length-=1
+      @length -= 1
       head = @head
       @head = @head.next
       head.value
     end
-
-
   end
 
   def peek
-    if head?
-      @head.value
-    end
+    @head.value if head?
   end
-
 end
 
 ll = Queue.new
@@ -61,3 +54,4 @@ ll.enqueue('thir item to join')
 p ll.deque
 
 p ll
+
